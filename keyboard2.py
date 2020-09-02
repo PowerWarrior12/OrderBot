8f81e0e6df840d49f035a7ef8aecb060590a545f
PowerWarrior12 <61107321+PowerWarrior12@users.noreply.github.com>
2020-09-02 17:15:42 +0300

    Delete keyboard2.py

--- keyboard2.py
+++
@@ -1,4 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton,ReplyKeyboardMarkup, KeyboardButton
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Оформить заказ'))
-keyboard2 = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Отменить'))
-keyboard3 = ReplyKeyboardMarkup(resize_keyboard=True).add(KeyboardButton('Показать заказы'))
